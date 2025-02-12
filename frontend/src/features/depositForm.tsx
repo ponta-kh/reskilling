3314135617bc664fd4558b5303e684b4a5d839bf
ponta-kh <kei.dev.itwork@gmail.com>
2025-02-12 09:24:41 +0900

    depositForm.tsx を更新
    一時的な変更

--- frontend/src/features/depositForm.tsx
+++ frontend/src/features/depositForm.tsx
@@ -5,13 +5,15 @@ import { useActionState } from "react";
 import Form from "next/form";
 
 export default function DepositForm() {
+    /*
     const [_, formAction, isPending] = useActionState(async (_, data: FormData) => {
         await new Promise((resolve) => setTimeout(resolve, 1000));
         console.log("depositVal");
     }, null);
+    */
 
     return (
-        <Form action={formAction} className="flex justify-center items-center gap-4 m-3">
+        <Form action="" className="flex justify-center items-center gap-4 m-3">
             <Input type="text" name="depositVal" placeholder={`100`} />
             <Button type="submit" variant="commit" disabled={isPending}>
                 預入
